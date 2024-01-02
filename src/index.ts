563ce959af7ede3de603252465f24a7cbc2e67f7
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-02 23:42:54 +0000

    feat: skip take example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import * as fs from 'node:fs';
 
-const data = fs.readFileSync("lines").toString().split("\n");
-data.forEach((line) => {
-    console.log(line);
-});
+fs.readFileSync("lines").
+    toString().
+    split("\n").
+    filter((_, i) => i % 2 == 0).
+    filter((_,i) => i >= 2 && i < 4).
+    forEach((line) => {
+        console.log(line);
+    });
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
-use std::collections::HashMap;
-
 fn main() {
-    let data = vec![1, 2, 3];
-    let mut foo = data.iter().map(|x| x + 1);
-    let mut new_vec = vec![];
-    // equivalent to collect()
-    while let Some(x) = foo.next() {
-        new_vec.push(x);
-    }
-    println!("{:?}", new_vec);
-
-    let how_many_times: usize = vec![1, 2, 3].into_iter().skip(2).count();
-    println!("{:?}", how_many_times);
-
-    vec![1, 2, 5, 9, 4]
-        .iter()
+    let file = std::fs::read_to_string("lines").unwrap();
+    file.lines()
+        .enumerate()
+        .filter(|(idx, _)| idx % 2 == 0)
         .skip(2)
-        .take_while(|&&x| x > 4)
-        .for_each(|x| println!("{}", x));
-
-    let hash_map = HashMap::from([("foo", 1), ("bar", 2), ("roo", 3)]);
-
-    hash_map.iter().for_each(|(k,v)| println!("{}:{}", k, v));  
+        .take(2)
+        .for_each(|(_, line)| println!("{}", line));
 }
