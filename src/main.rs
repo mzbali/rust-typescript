096297fe7e3a80359879f2abfc66c4ae61145c3c
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-04 23:40:31 +0000

    feat: seeing the borrow checker in action

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-fn main() {
-    let filename = std::env::args()
-        .nth(1)
-        .expect("Need a filename as argument");
-    let numbers = std::fs::read_to_string(filename).expect("Failed to read the file to string");
+#[derive(Debug)]
+struct Item {
+    count: isize,
+}
 
-    numbers.lines().for_each(|line| {
-        let num = line.parse::<usize>().ok();
-        if let Some(value) = num {
-            println!("{}", value);
-        } else {
-            println!("Not a number")
-        }
-    })
+fn add_one(item: &mut Item) {
+    item.count += 1;
+}
+
+fn main() {
+    let mut item = Item { count: 3 };
+    println!("{:?}", item);
+    add_one(&mut item);
+    println!("{:?}", item);
 }
