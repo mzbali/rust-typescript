41312e220224440b15c960c728aa1a5d26fec05f
Mahmud Zaman Bali <mahmudzbali@gmail.com>
2024-01-05 00:48:11 +0000

    feat: not immutable and mutable reference at the same time

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ fn add_one(item: &mut Item) {
     item.count += 1;
 }
 
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
+}
+
 fn main() {
-    let mut item = Item { count: 3 };
-    println!("{:?}", item);
-    add_one(&mut item);
-    println!("{:?}", item);
+    let mut items = vec![{ Item { count: 1 } }];
+    let first = items.first_mut();
+    println!("{:?}", first);
+    println!("{:?}", first);
 }
